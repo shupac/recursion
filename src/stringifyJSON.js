4381907ce1fe6323972b669a5817aa7339193172
Hongxu Liu <djshu.us@gmail.com>
2013-09-14 23:38:04 -0700

    Updated stringify to check if object being stringified is a function.
    Also added check to not return empty strings.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,39 +5,40 @@
 var stringifyJSON = function (obj) {
 	//var strArr = [obj];
     var strArr=[];
-    if(obj instanceof Array) {
-	  strArr.unshift("[");
-	  for(var i = 0; i < obj.length; i++) {
-		  strArr.push(stringifyJSON(obj[i]));
-          if(i!== obj.length-1) strArr.push(",");
-	  }
-	  strArr.push("]");
-	}
-	else if(obj instanceof String || typeof obj === 'string') {
-	  strArr.unshift("\"");
-      strArr.push(obj);
-	  strArr.push("\"");        
-	}
-    else if( Object.prototype.toString.call(obj) === '[object Object]' ) {
-      strArr.unshift("{");
-      for(var p in obj) {
-		if(obj[p]!==undefined && typeof obj[p]!== 'function') {
-			//only stringifies key-value pair if value is not undefined or a function
-            strArr.push(stringifyJSON(p));
-            strArr.push(":");
-	        if(typeof obj[p] === 'number') {
-	            strArr.push("\""+obj[p]+"\"");
+	if(typeof obj !== "function") {
+	    if(obj instanceof Array) {
+		  strArr.unshift("[");
+		  for(var i = 0; i < obj.length; i++) {
+			  strArr.push(stringifyJSON(obj[i]));
+	          if(i!== obj.length-1) strArr.push(",");
+		  }
+		  strArr.push("]");
+		}
+		else if(obj instanceof String || typeof obj === 'string') {
+		  strArr.unshift("\"");
+	      strArr.push(obj);
+		  strArr.push("\"");        
+		}
+	    else if( Object.prototype.toString.call(obj) === '[object Object]' ) {
+	      strArr.unshift("{");
+	      for(var p in obj) {
+			if(obj[p]!==undefined && typeof obj[p]!== 'function') {
+				//only stringifies key-value pair if value is not undefined or a function
+	            strArr.push(stringifyJSON(p));
+	            strArr.push(":");
+		        if(typeof obj[p] === 'number') {
+		            strArr.push("\""+obj[p]+"\"");
+		        }
+		        else strArr.push(stringifyJSON(obj[p]));
+		        strArr.push(",");
 	        }
-	        else strArr.push(stringifyJSON(obj[p]));
-	        strArr.push(",");
-        }
-        else if(strArr[strArr.length-1]===",") strArr.pop();
-      }
-      if(strArr[strArr.length-1]===",") strArr.pop();
-      strArr.push("}");
-    }
-    else if(obj === null) strArr.push("null");
-    else strArr.push(obj);
-
-	return strArr.join("");
+	        else if(strArr[strArr.length-1]===",") strArr.pop();
+	      }
+	      if(strArr[strArr.length-1]===",") strArr.pop();
+	      strArr.push("}");
+	    }
+	    else if(obj === null) strArr.push("null");
+	    else strArr.push(obj);
+	}
+	if(strArr.length>0) return strArr.join("");
 };
