4eaff4f85f6193de46ffed4dfd5eba5eccb4c6d6
Hongxu Liu <djshu.us@gmail.com>
2013-09-14 21:56:06 -0700

    Updated stringifyJSON to check if the object passed is an array or string. If array, function adds "[" and "]" to the string array. If string, "\"".

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,16 @@
 
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
-  // your code goes here
+	var strArr = [obj];
+	if(obj instanceof Array) {
+	  strArr.unshift("[");
+	  strArr.push("]");
+	}
+	else if(obj instanceof String || typeof obj === 'string') {
+	  strArr.unshift("\"");
+	  strArr.push("\"");        
+	}
+
+	return strArr.join("");
 };
+
