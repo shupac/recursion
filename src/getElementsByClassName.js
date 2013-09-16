5d56255465a2f4386631f384c283b0b2881e0ce4
Hongxu Liu <djshu.us@gmail.com>
2013-09-15 19:25:57 -0700

    Implemented getElementsByClassName by calling a second recursive function. Function passes separate testing using a different HTML but not tests in spec runner.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,5 +5,23 @@
 
 // But in stead we're going to implement it from scratch:
 var getElementsByClassName = function (className) {
-  // your code here
+	var array=[];
+	findNodes(document.childNodes, className, array);
+	return array;
 };
+
+var findNodes = function(node, className, array) {
+	for(var p in node) {
+		var name = node[p].className;
+		if(typeof name === 'string') {
+			if(name.indexOf("green") !== -1) {
+				//console.log(node[p]);
+				array.push(node[p]);				
+			}
+		}
+	}
+	for(var i = 0; i < node.length; i++) {		
+		findNodes(node[i].childNodes, className, array);
+	}
+};
+
