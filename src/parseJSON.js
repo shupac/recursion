b7ac9c0b7dc673d2b1e8331eba6a04eac09a53b0
Hack Reactor Students <github@hackreactor.com>
2013-09-23 22:18:18 -0700

    Parses primitive types and basic arrays. Does not work for arrays within arrays.

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,48 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function (json) {
-  // your code goes here
+  if(json==="null")  return null;
+  if(json==="true")  return true;
+  if(json==="false") return false;
+  if(!isArray(json) && !isObject(json)) {
+    if(isString(json)){
+      return json.substring(1,json.length-1) ;
+    }
+    else{
+      return +json;     // expect a number - checking for faulty input should also be here
+    }
+  }
+  else if(isArray(json)){
+    if(json === '[]') return [];
+    else {
+      json = json.substring(1,json.length-1);
+      var arr = json.split(",");
+      var retArr = [];
+      for(var i = 0; i < arr.length; i++) {
+        retArr.push(parseJSON(arr[i]));
+      }
+      return retArr;
+    }
+  }
+  else{       // expect object
+
+
+  }
 };
+
+
+
+
+
+
+var isString = function(json){
+  return (json[0]==='"');
+}
+
+var isArray = function(json){
+    return (json[0]==='[');
+}
+var isObject = function(json){
+  return false;//write this
+}
+
